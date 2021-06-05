80ed91781edf50330ef884a3cbe82246805b19e7
Amir <amirhatam136@gmail.com>
2021-06-05 10:36:42 +0200

    npm i react-particles-js

---
+++ canvas/particles/src/Particles.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+
+export default class Particles extends Component {
+  render() {
+    return (
+      <>
+        <Particles
+          // params={{
+          //   particles: {
+          //     number: {
+          //       value: 50,
+          //     },
+          //     size: {
+          //       value: 3,
+          //     },
+          //   },
+          //   interactivity: {
+          //     events: {
+          //       onhover: {
+          //         enable: true,
+          //         mode: "repulse",
+          //       },
+          //     },
+          //   },
+          // }}
+        />
+      </>
+    );
+  }
+}
+
---
+++ canvas/particles/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Particles from './Particles';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Particles />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
