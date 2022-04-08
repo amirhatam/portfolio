ad2ba977c8be3c998b3c2705758d231776fef122
Amir <amirhatam136@gmail.com>
2022-04-08 22:23:17 +0200

    change contact list in grid system

--- src/views/ContactPage.jsx
+++ src/views/ContactPage.jsx
@@ -68,54 +68,100 @@ const ContactPage = () => {
                   <MDBCol className="col-md-10 col-8 align-self-center px-0">
                     <span className="fontW h5-fs ml-1">Codewars</span>
                   </MDBCol>
-                  <MDBCol className="">
-                    <a href="https://www.codewars.com/users/AmirHatam" target="_blank" rel="noreferrer" className="fb-ic d-block txt-dark h6-fs textColor mb-md-0 my-lg-2 mt-1">https://www.codewars.com/users/AmirHatam</a>
+                  <MDBCol className="my-2">
+                    <a href="https://www.codewars.com/users/AmirHatam"
+                      target="_blank" rel="noreferrer"
+                      className="txt-dark h6-fs"
+                    >https://www.codewars.com/users/AmirHatam</a>
                   </MDBCol>
                 </MDBRow>
 
-                <div className="my-3">
-                  <MDBIcon fab icon="github" className="col-1 px-0 mr-1 h4-fs" />
-                  <span className="fontW h5-fs ml-1">GitHub</span>
-                  <div className="d-table">
-                    <a href="https://github.com/amirhatam" target="_blank" rel="noreferrer" className="fb-ic d-block txt-dark h6-fs textColor mb-md-0 my-lg-2 mt-1">https://github.com/amirhatam</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <MDBIcon fab icon="linkedin" className="col-1 px-0 mr-1 text-primary h4-fs" />
-                  <span className="fontW h5-fs ml-1">LINKEDIN</span>
-                  <div className="d-table">
-                    <a href="https://www.linkedin.com/in/amir-hatam-7ba7601ba/" target="_blank" rel="noreferrer" className="li-ic d-block txt-dark h6-fs textColor mb-md-0 my-lg-2 mt-1">https://www.linkedin.com/in/amir-hatam-7ba7601ba/</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <MDBIcon icon="camera-retro" className="col-1 px-0 mr-1 text-muted h4-fs" />
-                  <span className="fontW h5-fs ml-1">YouPic</span>
-                  <div className="d-table">
-                    <a href="https://youpic.com/photographer/AmirHatam/" target="_blank" rel="noreferrer" className="ins-ic d-block txt-dark h6-fs textColor mb-md-0 my-lg-2 mt-1">https://youpic.com/photographer/AmirHatam/</a>
-                  </div>
-                </div>
+                <MDBRow className="my-3">
+                  <MDBCol className="col-md-2 col-2 align-self-center pr-0">
+                    <MDBIcon fab icon="github" className="col-1 px-0 mr-1 h4-fs" />
+                  </MDBCol>
+                  <MDBCol className="col-md-10 col-8 align-self-center px-0">
+                    <span className="fontW h5-fs ml-1">GitHub</span>
+                  </MDBCol>
+                  <MDBCol className="my-2">
+                    <a href="https://github.com/amirhatam"
+                      target="_blank" rel="noreferrer"
+                      className="txt-dark h6-fs"
+                    >https://github.com/amirhatam</a>
+                  </MDBCol>
+                </MDBRow>
+
+                <MDBRow className="my-3">
+                  <MDBCol className="col-md-2 col-2 align-self-center pr-0">
+                    <MDBIcon fab icon="linkedin" className="col-1 px-0 mr-1 text-primary h4-fs" />
+                  </MDBCol>
+                  <MDBCol className="col-md-10 col-8 align-self-center px-0">
+                    <span className="fontW h5-fs ml-1">LINKEDIN</span>
+                  </MDBCol>
+                  <MDBCol className="my-2">
+                    <a href="https://www.linkedin.com/in/amir-hatam-7ba7601ba/"
+                      target="_blank" rel="noreferrer"
+                      className="li-ic txt-dark h6-fs">
+                      https://www.linkedin.com/in/amir-hatam-7ba7601ba/
+                    </a>
+                  </MDBCol>
+                </MDBRow>
+
+                <MDBRow className="my-3">
+                  <MDBCol className="col-md-2 col-2 align-self-center pr-0">
+                    <MDBIcon icon="camera-retro" className="col-1 px-0 mr-1 text-muted h4-fs" />
+                  </MDBCol>
+                  <MDBCol className="col-md-10 col-8 align-self-center px-0">
+                    <span className="fontW h5-fs ml-1">YouPic</span>
+                  </MDBCol>
+                  <MDBCol className="my-2">
+                    <a href="https://youpic.com/photographer/AmirHatam/"
+                      target="_blank" rel="noreferrer"
+                      className="ins-ic txt-dark h6-fs">
+                      https://youpic.com/photographer/AmirHatam/
+                    </a>
+                  </MDBCol>
+                </MDBRow>
               </MDBCol>
 
               <MDBCol className="col-lg-12 col-12">
-                <div className="mb-3">
-                  <MDBIcon icon="envelope" className="col-1 px-0 mr-lg-1 mr-2 text-muted h4-fs" />
-                  <span className="fontW h5-fs ml-1">Email</span>
-                  <div className="d-table">
-                    <a href="mailto:amirhatam136@gmail.com" className="d-block h6-fs txt-dark mb-md-0 my-lg-2 mt-1"> amirhatam136@gmail.com</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <MDBIcon icon="phone-alt" className="col-1 px-0 mr-1 text-info h4-fs" />
-                  <span className="fontW h5-fs ml-1">Téléphone </span>
-                  <div className="d-table">
-                    <a href="tel:+33 7 68 57 35 81" className="d-block h6-fs txt-dark mb-md-0 my-lg-2 mt-1">+33 7 68 57 35 81</a>
-                  </div>
-                </div>
-                <div className="my-3">
-                  <MDBIcon icon="map-marker-alt" className="col-1 text-danger h4-fs px-0 mr-1" />
-                  <span className="fontW h5-fs ml-1">Paris</span>
-                </div>
+
+                <MDBRow className="mb-3">
+                  <MDBCol className="col-md-2 col-2 align-self-center pr-0">
+                    <MDBIcon icon="envelope" className="col-1 px-0 mr-lg-1 mr-2 text-muted h4-fs" />
+                  </MDBCol>
+                  <MDBCol className="col-md-10 col-8 align-self-center px-0">
+                    <span className="fontW h5-fs ml-1">Email</span>
+                  </MDBCol>
+                  <MDBCol className="my-2">
+                    <a href="mailto:amirhatam136@gmail.com"
+                      className="d-block txt-dark h6-fs"
+                    > amirhatam136@gmail.com</a>
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow className="my-3">
+                  <MDBCol className="col-md-2 col-2 align-self-center pr-0">
+                    <MDBIcon icon="phone-alt" className="col-1 px-0 mr-1 text-info h4-fs" />
+                  </MDBCol>
+                  <MDBCol className="col-md-10 col-8 align-self-center px-0">
+                    <span className="fontW h5-fs ml-1">Téléphone </span>
+                  </MDBCol>
+                  <MDBCol className="my-2">
+                    <a href="tel:+33 7 68 57 35 81"
+                      className="d-block txt-dark h6-fs"
+                    >+33 7 68 57 35 81</a>
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow className="my-3">
+                  <MDBCol className="col-1 align-self-center">
+                    <MDBIcon icon="map-marker-alt" className="col-1 text-danger h4-fs px-0 mr-1" />
+                  </MDBCol>
+                  <MDBCol className="col-md-10 col-8 align-self-center px-0">
+                    <span className="fontW h5-fs ml-1">Paris</span>
+                  </MDBCol>
+                </MDBRow>
               </MDBCol>
+
             </MDBRow>
           </MDBCol>
         </MDBRow>
